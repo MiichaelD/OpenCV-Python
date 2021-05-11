a8576e66a3be658e8d9d1fa75043190d272dc868
Michael Duarte <michael.duarte@hotmail.com>
2021-05-10 21:31:01 -0700

    Updates gopro scripts + adds adaptive threshold

--- gopro_keepalive.py
+++ gopro_keepalive.py
@@ -2,4 +2,6 @@
 from goprocam import GoProCamera, constants
 
 gopro = GoProCamera.GoPro()
+# gopro.video_settings(res='1080p', fps='30')
+# gopro.gpControlSet(constants.Stream.WINDOW_SIZE, constants. Stream.WindowSize.R720)
 gopro.stream("udp://127.0.0.1:10000")
